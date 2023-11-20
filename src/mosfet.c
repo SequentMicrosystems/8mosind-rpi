36765b5bf7bf2ad7d198722797d0db715f5fb146
alexburcea2877 <olcitu@gmail.com>
2023-11-20 00:27:38 +0000

    Set the init value of the semaphore

--- src/mosfet.c
+++ src/mosfet.c
@@ -724,7 +724,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 #ifdef THREAD_SAFE
-	sem_t *semaphore = sem_open("/SMI2C_SEM", O_CREAT, 1);
+	sem_t *semaphore = sem_open("/SMI2C_SEM", O_CREAT, 0000666, 3);
 	int semVal = 2;
 #ifdef DEBUG_SEM
 	sem_getvalue(semaphore, &semVal);
